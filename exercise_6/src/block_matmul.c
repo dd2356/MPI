b4ca84f88ca8334608933fa34fa6f1221426bb70
Kristian Alvarez Jörgensen <krijor@kth.se>
2019-05-13 16:55:01 +0200

    Attempt to implement fox

--- exercise_6/src/block_matmul.c
+++ exercise_6/src/block_matmul.c
@@ -43,17 +43,100 @@ struct Config config;
 
 void init_matmul(char *A_file, char *B_file, char *outfile)
 {
-	int world_rank, world_size;
-	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &config.world_rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &config.world_size);
 
-	if (!is_square(world_size)) {
-		if (world_rank == 0) {
+	if (!is_square(config.world_size)) {
+		if (config.world_rank == 0) {
 			printf("Number of processes needs to be a perfect square\n");
 		}
 		exit(1);
 	}
 
+	MPI_File_open(MPI_COMM_WORLD, A_file, 
+		MPI_MODE_RDONLY, MPI_INFO_NULL, &config.A_file);
+	MPI_File_open(MPI_COMM_WORLD, B_file, 
+		MPI_MODE_RDONLY, MPI_INFO_NULL, &config.B_file);
+	MPI_File_open(MPI_COMM_WORLD, outfile, 
+		MPI_MODE_WRONLY, MPI_INFO_NULL, &config.C_file);
+	config.block = MPI_DOUBLE;
+
+    /* Copy output file name to configuration */
+    config.outfile = outfile; 
+
+	/* Get matrix size header */
+    if (config.world_rank == 0) { 
+        MPI_File_read_at(config.A_file, 0, &config.A_dims,
+                2, MPI_INT, MPI_STATUS_IGNORE);
+        MPI_File_read_at(config.B_file, 0, &config.B_dims,
+                2, MPI_INT, MPI_STATUS_IGNORE);
+    }
+	/* Broadcast global matrix sizes */
+    MPI_Bcast(config.A_dims,2,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Bcast(config.B_dims,2,MPI_INT,0,MPI_COMM_WORLD);
+
+    /* Set dim of tiles relative to the number of processes as NxN where N=sqrt(world_size) */
+	config.dim[0] = config.dim[1] = (int)sqrt(config.world_size);
+
+	/* Verify dim of A and B matches for matul and both are square*/
+	if (config.A_dims[0] != config.A_dims[1] 
+		|| config.B_dims[0] != config.B_dims[1]
+		|| config.C_dims[0] != config.C_dims[1]
+		|| config.A_dims[0] != config.B_dims[0]
+		|| config.A_dims[0] != config.C_dims[0]) {
+		if (config.world_rank == 0) {
+			printf("Matrices should be square and the same size\n");
+		}
+		exit(1);
+	}
+
+	if (config.A_dims[0] % config.dim[0] != 0 || config.A_dims[1] % config.dim[1] != 0) {
+		if (config.world_rank == 0) {
+			printf("Matrix not evenly divisible along grid\n");
+		}
+		exit(1);
+	}
+
+	/* Create Cart communicator for NxN processes */
+    int wrap_around[2] = {1,0}; 
+    MPI_Cart_create(MPI_COMM_WORLD,2,config.dim,wrap_around,1,&config.grid_comm); 
+
+    MPI_Comm_rank(config.grid_comm,&config.grid_rank); 
+    MPI_Cart_coords(config.grid_comm,config.grid_rank,2,config.coords);
+    
+	/* Sub div cart communicator to N row communicator */
+    int remain_dims[2] = {0,1}; // Keep row
+    MPI_Cart_sub(config.grid_comm,remain_dims,&config.row_comm); 
+
+    MPI_Comm_rank(config.row_comm,&config.row_rank); 
+    MPI_Comm_size(config.row_comm,&config.row_size); 
+
+	/* Sub div cart communicator to N col communicator */
+    remain_dims[0]=1; // Keep column
+    remain_dims[1]=0; // Keep column
+    MPI_Cart_sub(config.grid_comm,remain_dims,&config.col_comm); 
+
+    MPI_Comm_rank(config.col_comm,&config.col_rank); 
+    MPI_Comm_size(config.col_comm,&config.col_size); 
+
+	/* Setup sizes of full matrices */
+
+	/* Setup sizes of local matrix tiles */
+
+	/* Create subarray datatype for local matrix tile */
+
+	/* Create data array to load actual block matrix data */
+
+	/* Set fileview of process to respective matrix block */
+
+	/* Collective read blocks from files */
+
+	/* Close data source files */
+	// return;
+	MPI_File_close(&config.A_file);
+	MPI_File_close(&config.B_file);
+    
+    /* 
 	MPI_File_open(MPI_COMM_WORLD, A_file, 
 		MPI_MODE_RDONLY, MPI_INFO_NULL, &config.A_file);
 	MPI_File_open(MPI_COMM_WORLD, B_file, 
@@ -71,21 +154,7 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
 	config.matrix_size = config.C_dims[0] * config.C_dims[1];
 	// printf("A dims: %d x %d\n", config.A_dims[0], config.A_dims[1]);
 
-	config.grid_comm = MPI_COMM_WORLD;
-	MPI_Comm_split(MPI_COMM_WORLD, world_rank / config.A_dims[1], 
-		world_rank, &config.row_comm);
-	MPI_Comm_split(MPI_COMM_WORLD, world_rank % config.A_dims[1], 
-		world_rank, &config.col_comm);
-
-	config.world_rank = world_rank;
-	config.world_size = world_size;
-	config.grid_rank = world_rank;
-
-	config.dim[0] = config.dim[1] = (int)sqrt(world_size);
-	MPI_Comm_rank(config.row_comm, &config.row_rank);
-	MPI_Comm_rank(config.col_comm, &config.col_rank);
-	MPI_Comm_size(config.row_comm, &config.row_size);
-	MPI_Comm_size(config.col_comm, &config.col_size);
+	config.dim[0] = config.dim[1] = (int)sqrt(config.world_size);
 
 	if (config.A_dims[0] != config.A_dims[1] 
 		|| config.B_dims[0] != config.B_dims[1]
@@ -145,31 +214,8 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
 	// make sure that A_tmp and B_tmp have the correct data to begin with (could be done with MPI)
 	memcpy(config.A_tmp, config.A, config.local_size * sizeof(double));
 	memcpy(config.B_tmp, config.B, config.local_size * sizeof(double));
+    */
 
-	/* Verify dim of A and B matches for matul and both are square*/
-
-	/* Create Cart communicator for NxN processes */
-
-	/* Sub div cart communicator to N row communicator */
-
-	/* Sub div cart communicator to N col communicator */
-
-	/* Setup sizes of full matrices */
-
-	/* Setup sizes of local matrix tiles */
-
-	/* Create subarray datatype for local matrix tile */
-
-	/* Create data array to load actual block matrix data */
-
-	/* Set fileview of process to respective matrix block */
-
-	/* Collective read blocks from files */
-
-	/* Close data source files */
-	// return;
-	MPI_File_close(&config.A_file);
-	MPI_File_close(&config.B_file);
 }
 
 void cleanup_matmul()
@@ -207,8 +253,8 @@ void multiply() {
 		for (int n = 0; n < config.local_dims[1]; n++) {
 			for (int k = 0; k < config.local_dims[1]; k++) {
 				config.C[m * config.local_dims[1] + n] 
-					+= config.A_tmp[m * config.local_dims[1] + k] 
-					* config.B_tmp[k * config.local_dims[1] + n];
+					+= config.A[m * config.local_dims[1] + k] 
+					* config.B[k * config.local_dims[1] + n];
 			}
 		}
 	}
@@ -216,16 +262,31 @@ void multiply() {
 
 void compute_fox()
 {
-
+    MPI_Status status;
+    int root, source, dest; 
 	/* Compute source and target for verticle shift of B blocks */
+    source = (config.row_rank + 1) % config.dim[0]; 
+    dest = (config.row_rank + config.dim[0] -1) % config.dim[0]; 
 
 	for (int i = 0; i < config.dim[0]; i++) {
-		multiply();
 		/* Diag + i broadcast block A horizontally and use A_tmp to preserve own local A */
+        root = (config.col_rank + i) % config.dim[0]; 
+        memcpy(config.A_tmp,config.A,sizeof(double)*config.local_size); 
+        MPI_Bcast(config.A,config.matrix_size,config.block,root,config.row_comm);
 
 		/* dgemm with blocks */
-		
+        multiply();
 		/* Shfting block B upwards and receive from process below */
+        MPI_Sendrecv_replace(
+                config.B_tmp,
+                config.local_size,
+                config.block,
+                dest,
+                0,
+                source,
+                0,
+                config.col_comm,
+                &status);	
 
 	}
 }
