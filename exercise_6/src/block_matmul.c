740729b7d56f85a8752e6beed7137c13b0cd8bd2
Kristian Alvarez Jörgensen <krijor@kth.se>
2019-05-14 12:59:44 +0200

    Finish init

--- exercise_6/src/block_matmul.c
+++ exercise_6/src/block_matmul.c
@@ -45,6 +45,9 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
 {
 	MPI_Comm_rank(MPI_COMM_WORLD, &config.world_rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &config.world_size);
+    if (config.world_rank == 0) { 
+        printf("Size: %d\n",config.world_size); 
+    }
 
 	if (!is_square(config.world_size)) {
 		if (config.world_rank == 0) {
@@ -59,7 +62,6 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
 		MPI_MODE_RDONLY, MPI_INFO_NULL, &config.B_file);
 	MPI_File_open(MPI_COMM_WORLD, outfile, 
 		MPI_MODE_WRONLY, MPI_INFO_NULL, &config.C_file);
-	config.block = MPI_DOUBLE;
 
     /* Copy output file name to configuration */
     config.outfile = outfile; 
@@ -71,9 +73,15 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
         MPI_File_read_at(config.B_file, 0, &config.B_dims,
                 2, MPI_INT, MPI_STATUS_IGNORE);
     }
+
+    /* Calculate result matrix dimensions */ 
+    config.C_dims[0]=config.A_dims[0];
+    config.C_dims[1]=config.B_dims[1];
+
 	/* Broadcast global matrix sizes */
     MPI_Bcast(config.A_dims,2,MPI_INT,0,MPI_COMM_WORLD);
     MPI_Bcast(config.B_dims,2,MPI_INT,0,MPI_COMM_WORLD);
+    MPI_Bcast(config.C_dims,2,MPI_INT,0,MPI_COMM_WORLD);
 
     /* Set dim of tiles relative to the number of processes as NxN where N=sqrt(world_size) */
 	config.dim[0] = config.dim[1] = (int)sqrt(config.world_size);
@@ -120,16 +128,50 @@ void init_matmul(char *A_file, char *B_file, char *outfile)
     MPI_Comm_size(config.col_comm,&config.col_size); 
 
 	/* Setup sizes of full matrices */
+    config.matrix_size = config.A_dims[0] * config.B_dims[1]; 
 
 	/* Setup sizes of local matrix tiles */
+	config.local_dims[0] = config.A_dims[0] / config.dim[0];
+	config.local_dims[1] = config.A_dims[1] / config.dim[1];
+	config.local_size = config.local_dims[0] * config.local_dims[1];
+    if (config.world_rank == 0) { 
+        printf("local dims: %d x %d\n", config.local_dims[0], config.local_dims[1]);
+    }
 
 	/* Create subarray datatype for local matrix tile */
+	config.block = MPI_DOUBLE;
 
 	/* Create data array to load actual block matrix data */
+	config.A = (double*)malloc(sizeof(double) * config.local_size);
+	config.A_tmp = (double*)malloc(sizeof(double) * config.local_size);
+	config.B = (double*)malloc(sizeof(double) * config.local_size);
+	config.B_tmp = (double*)malloc(sizeof(double) * config.local_size);
+	config.C = (double*)calloc(sizeof(double), config.local_size);
 
 	/* Set fileview of process to respective matrix block */
 
 	/* Collective read blocks from files */
+	for (int i = 0; i < config.local_dims[0]; i++) {
+		int row_offset = i * config.A_dims[0] 
+			+ config.col_rank * config.local_dims[0] * config.A_dims[1];
+		int col_offset = config.local_dims[1] * config.row_rank;
+		int array_offset_bytes = (col_offset + row_offset) * sizeof(double);
+		int start_index = 2 * sizeof(int) + array_offset_bytes;
+
+		MPI_File_read_at(config.A_file, start_index, 
+			&config.A[i * config.local_dims[1]], config.local_dims[1], 
+			MPI_DOUBLE, MPI_STATUS_IGNORE);
+		MPI_File_read_at(config.B_file, start_index, 
+			&config.B[i * config.local_dims[1]], config.local_dims[1], 
+			MPI_DOUBLE, MPI_STATUS_IGNORE);
+	}
+    if (config.world_rank == 0) { 
+        printf("Finished reading files\n"); 
+    }
+    
+	for (int i = 0; i < config.local_size; i++) {
+		//printf("%d: A %d: %f\n", config.world_rank, i, config.A[i]);
+	}
 
 	/* Close data source files */
 	// return;
@@ -249,6 +291,7 @@ void cleanup_matmul()
 
 // Uses the config struct exclusively, does not need any arguments
 void multiply() {
+    printf("[ %d ] Multiplying\n",config.world_rank); 
 	for (int m = 0; m < config.local_dims[0]; m++) {
 		for (int n = 0; n < config.local_dims[1]; n++) {
 			for (int k = 0; k < config.local_dims[1]; k++) {
@@ -272,11 +315,17 @@ void compute_fox()
 		/* Diag + i broadcast block A horizontally and use A_tmp to preserve own local A */
         root = (config.col_rank + i) % config.dim[0]; 
         memcpy(config.A_tmp,config.A,sizeof(double)*config.local_size); 
-        MPI_Bcast(config.A,config.matrix_size,config.block,root,config.row_comm);
+        MPI_Bcast(config.A,config.local_size,config.block,root,config.row_comm);
+
 
 		/* dgemm with blocks */
         multiply();
+
+        /* reset A from tmp */
+        memcpy(config.A,config.A_tmp,sizeof(double)*config.local_size); 
+
 		/* Shfting block B upwards and receive from process below */
+        memcpy(config.B_tmp,config.B,sizeof(double)*config.local_size); 
         MPI_Sendrecv_replace(
                 config.B_tmp,
                 config.local_size,
@@ -287,6 +336,7 @@ void compute_fox()
                 0,
                 config.col_comm,
                 &status);	
+        memcpy(config.B,config.B_tmp,sizeof(double)*config.local_size); 
 
 	}
 }
